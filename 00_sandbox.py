2641ba67301476a33016c3f218463ccd1d4dfbb6
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2022-02-16 14:28:08 +1300

    Update 00_sandbox.py

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,1 +1,8 @@
-print ("hello world")
+# Ask users for a number
+get_number = int(input("ligma?"))
+
+# Multiply the number by five
+times_five = get_number * 5
+
+# Output the result
+print(" {} times five is equal to {}".format(get_number, times_five ))
